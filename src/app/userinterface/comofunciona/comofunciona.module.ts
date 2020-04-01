1313b9c41863767d15e564254f2ff045c4207b5d
fgmendezg <fgmendezg@unal.edu.co>
2020-04-01 11:21:13 -0500

    como funciona con paso a paso

--- src/app/userinterface/comofunciona/comofunciona.module.ts
+++ src/app/userinterface/comofunciona/comofunciona.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComofuncionaComponent } from './comofunciona.component';
+import { NbStepperModule, NbButtonModule, NbCardModule } from '@nebular/theme';
 
 
 
 @NgModule({
   declarations: [ComofuncionaComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NbStepperModule,
+    NbButtonModule,
+    NbCardModule
   ],
   exports: [
     ComofuncionaComponent
-  ]
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class ComofuncionaModule { }
