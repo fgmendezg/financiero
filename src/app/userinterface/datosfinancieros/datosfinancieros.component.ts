8b40dcd677e4ae6c8493a2f97f4b8ceec781f399
fgmendezg <fgmendezg@unal.edu.co>
2020-04-23 03:00:15 -0500

    se cuadra el menu principal, además se agrego el contenido de datos financieros

--- src/app/userinterface/datosfinancieros/datosfinancieros.component.ts
+++ src/app/userinterface/datosfinancieros/datosfinancieros.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatosfinancierosComponent implements OnInit {
 
-  constructor() { }
+  sector = [];
+  convenios = [];
+
+  constructor() { 
+    this.sector = [
+      "Fuerzas Armadas",
+      "Salud",
+      "Edicación",
+      "Administrativo",
+      "Judicial",
+      "Otro"
+    ];
+    this.convenios = [
+      "Convenio 1",
+      "Convenio 2",
+      "Convenio 3",
+      "Convenio 4",
+      "Convenio 5",
+      "Convenio 6"
+    ];
+  }
 
   ngOnInit(): void {
+  }
+
+  //TODO: envia los datos cargados en el formulario
+  // Primero deberia mostrar un resumen de lo que va a enviar.
+  enviarDatos(){
+
   }
 
 }
